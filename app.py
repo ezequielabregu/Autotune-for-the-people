ca097c074cabf792d731c9d5aa13f92c01868e63
ezequielabregu <ezequielabregu.code@gmail.com>
2023-03-18 11:37:04 +0100

    change port listening to 10000

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def main_at(audio_input):
     #sf.write(str(output_filepath), pitch_corrected_y, sr)
 
 if __name__ == '__main__':
-    app.run(debug=False)
-#    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
+#    app.run(debug=False)    
 #    app.run(debug=True)
         
