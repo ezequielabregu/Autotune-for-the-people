8e1c1465bdf22f217cfce890f3d6f0cf755520b8
ezequielabregu <ezequielabregu.code@gmail.com>
2023-03-18 13:18:13 +0100

    change app.run()

--- app.py
+++ app.py
@@ -141,7 +141,8 @@ def main_at(audio_input):
     #sf.write(str(output_filepath), pitch_corrected_y, sr)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    app.run()
+#    app.run(host="0.0.0.0", port=10000)
 #    app.run(debug=False)    
 #    app.run(debug=True)
         
